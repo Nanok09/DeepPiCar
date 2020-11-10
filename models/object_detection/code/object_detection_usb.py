45fe0ff80de24113c8f281ea4c7cbff13006d0d7
Nanok09 <matyikiraly9@gmail.com>
2020-11-10 17:32:15 +0100

    New model with 59 images

--- models/object_detection/code/object_detection_usb.py
+++ models/object_detection/code/object_detection_usb.py
@@ -65,6 +65,9 @@ def main():
                     print('can NOT read from camera')
                     break
 
+                frame_expanded = np.expand_dims(frame, axis=0)
+
+                ret, img = camera.read()
                 input = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # convert to RGB color space
                 img_pil = Image.fromarray(input)
                 #input = cv2.resize(input, (width,height))
